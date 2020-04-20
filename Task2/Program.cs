29b39924ef59b09ad46a10fc963fd27b391c1b00
Loarlam <messiah02@mail.ru>
2020-04-20 13:22:03 +0300

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task2
+{
+    class CarCollection<T>
+    {
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
